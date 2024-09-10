b8c478ba445b6797c6def2c56971e9e8865cf733
Elian Miranda <elianmir@outlook.com>
2024-09-10 13:22:47 -0300

    ejercicio 1 resuelto

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
-  return <div></div>;
+  return <section className='text-center'>
+    <h1 className='mt-3'>Hello World!</h1>
+  </section>;
 }
 
 export default App;
